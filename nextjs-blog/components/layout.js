395eee222d715a32e55c8353ff46ae33665aeef0
jin hye <sjrry112@gmail.com>
2023-01-06 16:00:39 +0900

    230106_1st_Commit

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from './layout.module.css'
+import Image from 'next/image';
 
 const Layout = ({children, home}) => {
   return (  
@@ -9,9 +10,31 @@ const Layout = ({children, home}) => {
         <title>Layout next</title>
       </Head>
       <header>
-                { home ? <h1>home</h1> : <h1>not home</h1>}
-            </header>
+      { home ? 
+                <div>
+                    <Image
+                    src='/images/profile.jpg'
+                    height={144}
+                    width={144}
+                    alt="프로필사진"
+                    ></Image>
+                    <h2>Green blog</h2>
+                </div>
+                :                 <div>
+                <Image
+                src='/images/profile.jpg'
+                height={100}
+                width={100}
+                alt="프로필사진"
+                ></Image>
+                <h2>Green blog post</h2>
+            </div>
+            }
+        </header>
       <div>{children}</div>
+      <footer>
+      <p>개인 블로그입니다</p>
+      </footer>
     </div>
 
   );
--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -2,66 +2,43 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 import Layout from '../components/layout';
+// 정적 생성을 위해 작성
+import { getSortedPostsData } from '../lib/posts';
+import { useState } from 'react';
 
-export default function Home() {
+// 정적 생성을 위해서 getStaticPropse생성
+export async function getStaticProps() {
+  const allPostData = getSortedPostsData();
+  return {
+    props : {
+      // 객체안에 allPostData : [포스트데이터] 형식으로 들어감
+      allPostData,
+    }
+  }
+}
+
+
+export default function Home({allPostData}) {
+  const [name, setName] = useState("홍길동");
   return (
     <Layout home>
-      <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Read <Link href="/posts/first-post">this page!</Link>
-        </h1>
+        <h2> {name} Next.js를 이용하여 연습하는 중입니다</h2>
+        <Link href='/posts/first-post'>포스트로 이동</Link>
+        <ul>
+          {
+            allPostData.map(({id, date, title})=>(
+              <li>{title} {id} {date}</li>
+            ))
+          }
+        </ul>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
 
       <style jsx>{`
         main {
@@ -113,8 +90,6 @@ export default function Home() {
           box-sizing: border-box;
         }
       `}</style>
-    </div>
     </Layout>
-    
   )
 }
